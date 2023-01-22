5e2a638c1e1a647cf392c51a6ab08e35a57c69f8
Pranavmath <pranav.sambhu@gmail.com>
2023-01-22 16:33:32 -0500

    Changed

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from app import *
 import math
 from flask_socketio import *
 
+
 """
 Note: Since the model files are really big git lfs was used. 
 Since an error was originally thrown after using git lfs this stack overflow article was used to fix the error:
